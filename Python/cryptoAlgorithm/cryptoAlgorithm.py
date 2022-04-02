e3debbd306e5c75637bf198c9472e715e18d79fb
Thomas.Zhang <Thomas.Zhang@Honeywell.com>
2022-04-02 11:18:53 +0800

    XO encryption and decrytion functions

--- Python/cryptoAlgorithm/cryptoAlgorithm.py
+++ Python/cryptoAlgorithm/cryptoAlgorithm.py
@@ -29,3 +29,26 @@ class MoveAlgorithm():
             tempDes = tempDes + tempChar
         return tempDes
 
+class XORAlgorithm():
+    def __init__(self, step, factor):
+        self.factor = ord(factor)
+
+    def encrypt(self, srcCode):
+        tempSrc = ''
+        for eachChar in srcCode:
+            tempAscii = ord(eachChar)
+            tempAscii = tempAscii ^ self.factor
+        return tempSrc
+
+    def decrypt(self, desCode):
+        tempDes = ''
+        for eachChar in desCode:
+            tempAscii = ord(eachChar) - self.step
+            if tempAscii > self.min:
+                tempChar = chr(tempAscii)
+            else:
+                tempChar = chr(self.max - (self.min - tempAscii) - 1)
+
+            tempDes = tempDes + tempChar
+        return tempDes
+
